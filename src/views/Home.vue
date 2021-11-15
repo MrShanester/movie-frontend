1f1f593eee1384ebff08ac3fa2421268e278b6eb
Shane Nicholson <shanescottnicholson@gmail.com>
2021-11-15 12:13:45 -0800

    init commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>{{ movie.title }}</h2>
+
+      <img v-bind:src="movie.image" v-bind:alt="movie.title" />
+
+      <p>{{ movie.body }}</p>
+      <p></p>
+      <p>---------------------------------</p>
+    </div>
   </div>
 </template>
 
+<style scoped>
+h1 {
+  text-align: center;
+  border: 12px solid;
+  border-image: linear-gradient(135deg, #ff0000 0%, #49ff33 25%, #3364ff 50%, #f6ff33 75%, #ff00a8 100%) 1;
+}
+
+img {
+  width: 250px;
+  height: 350px;
+}
+
+h2 {
+  font-style: italic;
+  border-top-style: dashed;
+  border-bottom-style: dashed;
+  border-left-style: solid;
+  border-right-style: solid;
+}
+
+.home {
+  margin: 0 auto;
+  padding: 10px 25px;
+  border: 3px solid;
+  border-image: linear-gradient(135deg, #ff0000 0%, #49ff33 25%, #3364ff 50%, #f6ff33 75%, #ff00a8 100%) 1;
+
+  background: rgb(228, 226, 226);
+}
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Movies",
+      moviess: [],
+    };
+  },
+  created: function () {
+    this.indexMoviess();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("/movies").then((response) => {
+        this.movies = response.data;
+      });
+    },
   },
 };
 </script>
