1457a4359753c0be99ab0bf087c8582072e20d59
Shane Nicholson <shanescottnicholson@gmail.com>
2021-11-22 18:52:27 -0800

    Adds dynamic log in/log out buttons depending on whether or not the user is logged in

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,14 @@
       |
       <router-link to="/newmovie">Add Movie</router-link>
       |
-      <router-link to="/signup">Sign Up</router-link>
-      |
-      <router-link to="/login">Log In</router-link>
-      |
-      <router-link to="/logout">Log Out</router-link>
+      <span v-if="isLoggedIn()">
+        <router-link to="/logout">Logout</router-link>
+      </span>
+      <span v-else>
+        <router-link to="/signup">Signup</router-link>
+        |
+        <router-link to="/login">Login</router-link>
+      </span>
     </div>
     <router-view />
   </div>
@@ -37,3 +40,13 @@
   color: #42b983;
 }
 </style>
+
+<script>
+export default {
+  methods: {
+    isLoggedIn: function () {
+      return localStorage.getItem("jwt");
+    },
+  },
+};
+</script>
